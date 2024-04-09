ce0ee43f2cf781db2ad3b00f9e8a45c006f6dd30
Fabi <fabibause12@gmail.com>
2024-04-09 22:19:49 +0200

    würfel2

--- js/wuerfeln.js
+++ js/wuerfeln.js
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function() {
     cube.addEventListener("click", function() {
         const random1 = Math.floor(Math.random() * 6) + 1 + 6*2; // Zufällige Seite zwischen 1 und 6 auswählen
         const random2 = Math.floor(Math.random() * 6) + 1 + 6*2; // Zufällige Seite zwischen 1 und 6 auswählen
-        const degreesToRotateY = 360 * 5 + (90 * (random1 - 1)); // Mindestens 1800 Grad plus die Drehung zur ausgewählten Seite
-        const degreesToRotateX = 360 * 5 + (90 * (random2 - 1)); // Mindestens 1800 Grad plus die Drehung zur ausgewählten Seite
+        const degreesToRotateY = 360 * 13 + (90 * (random1 - 1)); // Mindestens 1800 Grad plus die Drehung zur ausgewählten Seite
+        const degreesToRotateX = 360 * 17 + (90 * (random2 - 1)); // Mindestens 1800 Grad plus die Drehung zur ausgewählten Seite
         cube.style.transform = `rotateY(${degreesToRotateY}deg) rotateX(${degreesToRotateX}deg)`; // Drehung zum ausgewählten Winkel
         /*cube.style.transform = `rotateX(${degreesToRotateX/2}deg)`; // Drehung zum ausgewählten Winkel 
         cube.style.transform = `rotateY(${degreesToRotateY/2}deg)`; // Drehung zum ausgewählten Winkel
